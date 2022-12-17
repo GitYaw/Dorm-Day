2d4bbef58d84e55da01e71cfd8138ad7cfabf009
DavidFrempong <onlyonegraphics20@gmail.com>
2022-12-17 17:37:34 -0500

    new

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-// Variables
+// Variables (We set here the "ID" of the element. We've also created a variable, with the ending "changed", that will help us toggle the prices. In other words, if a button is clicked once, that price will add to the total. If it's clicked again, it will subtract.)
 let ramen = document.getElementById('ramen');
+let ramenchanged = false;
 let ramencost = parseInt(ramen.value);
 
+let water = document.getElementById('water');
+let waterchanged = false;
+let watercost = parseInt(water.value);
+
+// This variable is the final cost of all items added together
 let finalcost = document.getElementById('final-cost');
 let finalcostValue = finalcost.innerHTML;
 
-console.log(finalcostValue);
-
 let costOfItems = (ramencost).toFixed(2);
 
-// Functions
+// These functions are saying: "If the button is clicked, set the changed variable to the opposite of what it was before (change it from false to true)." Then we are saying: "If it's true that it's clicked, add that price to the total cost, but if it's false, subtract it from itself to make it 0 (Ex: 10 - 10 = 0)."
 ramen.addEventListener('click', function(){
+    ramenchanged = !ramenchanged;
 
-    finalcost.innerHTML = costOfItems;
-
+    finalcost.innerHTML = ((ramenchanged ? ramencost : ramencost - ramencost) + (waterchanged ? watercost : watercost - watercost)).toFixed(2);
 })
+
+water.addEventListener('click', function(){
+    waterchanged = !waterchanged;
+
+    finalcost.innerHTML = ((ramenchanged ? ramencost : ramencost - ramencost) + (waterchanged ? watercost : watercost - watercost)).toFixed(2);
+})
+
